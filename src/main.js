4ec3e1f332a41eff2484cf3c5895a27ccb7260a4
DawidGawronskiDev <dawidgawronskicontact@gmail.com>
2023-09-03 19:52:12 +0100

    Add app init

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
 import "./style.css";
+
+const root = document.querySelector("#root");
+import { render } from "./utils/renderUtils";
+import setup from "./utils/setup";
+import Data from "./data.json";
+
+render(root);
+setup();
+
+console.log(Data);
