9fba1ddd882397635d713e86645be88e3cbda994
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2023-09-11 13:59:09 +0100

    Refactor SubtaskDetail.js

---
+++ src/components/Subtasks/SubtasksDetail.js
@@ -0,0 +1,33 @@
+import { createElement } from "../../main";
+
+const createSubtasksDetail = (container, task) => {
+    const subtasksTitle = createElement("span", ["subtask-title", "body-m"]);
+  subtasksTitle.innerHTML = "Subtasks";
+  container.appendChild(subtasksTitle);
+
+  task.subtasks.forEach((subtask) => {
+    const subtaskContainerItem = createElement("div", ["input-container"]);
+    subtaskContainerItem.dataset.isCompleted = subtask.isCompleted;
+    container.appendChild(subtaskContainerItem);
+
+    const subtaskCheckbox = createElement("input", ["subtask-checkbox"]);
+    subtaskCheckbox.type = "checkbox";
+    if (subtask.isCompleted) subtaskCheckbox.checked = true;
+    subtaskContainerItem.appendChild(subtaskCheckbox);
+
+    subtaskCheckbox.addEventListener("click", (e) => {
+      e.target.checked === true
+        ? (subtask.isCompleted = true)
+        : (subtask.isCompleted = false);
+
+      subtaskContainerItem.dataset.isCompleted = e.target.checked;
+    });
+
+    const taskLabel = document.createElement("label");
+    taskLabel.classList.add("task-label");
+    taskLabel.innerHTML = subtask.title;
+    subtaskContainerItem.appendChild(taskLabel);
+  });
+}
+
+export default createSubtasksDetail;
